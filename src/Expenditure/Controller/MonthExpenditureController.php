6e9003acb849a63b63e60380db2a8d52d87a31c9
Nigel Greenway <nigel_greenway@me.com>
2013-08-04 22:57:12 +0100

    Removal of trailing whitespace.

--- src/Expenditure/Controller/MonthExpenditureController.php
+++ src/Expenditure/Controller/MonthExpenditureController.php
@@ -15,22 +15,22 @@ class MonthExpenditureController extends BaseController
      * @return 1
      */
     public function paidAction($expenditureID, Request $request)
-    {   
+    {
         $expenditure = $this->db->first('Expenditure\Model\MonthExpenditure', array('id' => $expenditureID));
-        
+
         $amount = $request->get('amount');
-        
+
         $expenditure->amount_paid = ($amount == 'all'? $expenditure->price: floatval($amount));
-        
+
         $this->db->save($expenditure);
-        
+
         return 1;
     }
-    
+
     /**
      * Save a new expenditure item
      *
-     * @param integer $headerID The header 
+     * @param integer $headerID The header
      * @param Request $request
      * @return RedirectResponse
      */
@@ -39,9 +39,9 @@ class MonthExpenditureController extends BaseController
         $expenditureID = $request->get('expenditureID');
         $title = $request->get('title');
         $price = $request->get('price');
-    
+
         if (strlen($title) > 0 && strlen($price) > 0) {
-        
+
             if ($expenditureID > 0) {
                 $expenditure = $this->db->first('Expenditure\Model\MonthExpenditure', array('id' => $expenditureID));
             } else {
@@ -51,13 +51,13 @@ class MonthExpenditureController extends BaseController
             $expenditure->title = $title;
             $expenditure->price = $price;
             $expenditure->header_id = $headerID;
-    
+
             $this->db->save($expenditure);
         }
-    
+
         return new RedirectResponse('/', 302);
     }
-    
+
     /**
      * Delete an expenditure item
      *
@@ -67,9 +67,9 @@ class MonthExpenditureController extends BaseController
     public function deleteAction($expenditureID)
     {
         $expenditure = $this->db->first('Expenditure\Model\MonthExpenditure', array('id' => $expenditureID));
-        
+
         $this->db->delete($expenditure);
-    
+
         return 1;
     }
 }
