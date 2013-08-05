cab5e24a9c6f51fcca2cb8bc670965b26027e0de
Nigel Greenway <nigel_greenway@me.com>
2013-08-05 13:33:09 +0100

    Removed trailing whitespace.

--- src/Expenditure/Controller/ImportController.php
+++ src/Expenditure/Controller/ImportController.php
@@ -20,25 +20,25 @@ class ImportController extends BaseController
         $monthHeader = new \Expenditure\Model\MonthHeader;
         $monthHeader->calendar_date = $this->getCarbon()->createFromDate($year, $month, 1)->toDateString();
         $monthHeader->month_income = $request->get('salary');
-        
+
         $this->db->save($monthHeader);
-        
+
         $monthlyTemplates = $this->db->all('Expenditure\Model\MonthExpenditureTemplate');
-        
+
         if (count($monthlyTemplates) > 0) {
-            
+
             foreach ($monthlyTemplates as $monthlyTemplate) {
-            
+
                 $monthExpenditure = new \Expenditure\Model\MonthExpenditure;
                 $monthExpenditure->title = $monthlyTemplate->title;
                 $monthExpenditure->price = $monthlyTemplate->price;
                 $monthExpenditure->amount_paid = 0;
                 $monthExpenditure->header_id = $monthHeader->id;
-            
+
                 $this->db->save($monthExpenditure);
             }
         }
-        
+
         return new RedirectResponse('/', 302);
     }
 }
